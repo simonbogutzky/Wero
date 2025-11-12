68f548fbd01b2a990838caa731b712161d17f73d
Simon Bogutzky <s.bogutzky@reply.de>
2025-11-12 14:03:09 +0100

    Fixes colors

--- Wero/Extensions/Color+Sparkassen.swift
+++ Wero/Extensions/Color+Sparkassen.swift
@@ -8,28 +8,6 @@
 import SwiftUI
 
 extension Color {
-    // MARK: - Primary Brand Colors
-
-    /// Primary Sparkassen brand color (RGB 255/0/0)
-    static let sparkassenRed = Color("SparkassenRed")
-
-    /// Secondary brand color for text and UI elements
-    static let sparkassenDarkGray = Color("SparkassenDarkGray")
-
-    /// Secondary brand color for professional accents
-    static let sparkassenDarkBlue = Color("SparkassenDarkBlue")
-
-    // MARK: - Accent Colors
-
-    /// Accent color for highlights and notifications
-    static let sparkassenOrange = Color("SparkassenOrange")
-
-    /// Accent color for rewards and achievements
-    static let sparkassenYellow = Color("SparkassenYellow")
-
-    /// Accent color for information and secondary actions
-    static let sparkassenLightBlue = Color("SparkassenLightBlue")
-
     // MARK: - Loyalty Level Colors
 
     /// Bronze level color
